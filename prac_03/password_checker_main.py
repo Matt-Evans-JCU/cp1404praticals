628ef961235d54fb08dca7dd742969d16a13d65b
Matt <matt.evans@my.jcu.edu.au>
2019-08-20 09:19:32 +1000

    Add main function

--- prac_03/password_checker_main.py
+++ prac_03/password_checker_main.py
@@ -1,5 +1,8 @@
-password = input("Pick a Password: ")
-while len(password) < 6:
-    print("Password not long enough")
-    password = input("Pick another Password: ")
-print('*' * len(password))
+def main():
+    password = input("Pick a Password: ")
+    while len(password) < 6:
+        print("Password not long enough")
+        password = input("Pick another Password: ")
+    print('*' * len(password))
+
+main()
