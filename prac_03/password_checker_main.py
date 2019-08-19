6f305db5c42d3cd4e1e7f3c97697ff4843bfeb25
Matt <matt.evans@my.jcu.edu.au>
2019-08-20 09:24:43 +1000

    Refactor password checker to function

--- prac_03/password_checker_main.py
+++ prac_03/password_checker_main.py
@@ -1,8 +1,14 @@
 def main():
-    password = input("Pick a Password: ")
-    while len(password) < 6:
-        print("Password not long enough")
-        password = input("Pick another Password: ")
+    password = get_password()
     print('*' * len(password))
+
+
+def get_password():
+    password_choice = input("Pick a Password: ")
+    while len(password_choice) < 6:
+        print("Password not long enough")
+        password_choice = input("Pick another Password: ")
+    return password_choice
+
 
 main()
