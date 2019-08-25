2487baf892653bdc933803ee780cdc5b539de5be
Matt <matt.evans@my.jcu.edu.au>
2019-08-25 15:31:03 +1000

    Add username check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,6 +10,11 @@ def main():
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
     print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
-
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    username = input("What is your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 main()
