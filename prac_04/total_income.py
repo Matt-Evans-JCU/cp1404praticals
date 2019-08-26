5fca6338db8eb33dcf1c7853d7bea74fa03a30be
Matt <matt.evans@my.jcu.edu.au>
2019-08-27 09:52:00 +1000

    Update def docstrings and function parameters

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,20 +5,21 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of months."""
+    """Asks for number of months and collects income and prints report through function"""
     incomes = []
     number_of_months = int(input("How many months? "))
 
     for month in range(1, number_of_months + 1):
-        income = float(input("Enter income for month {}:".format(str(month))))
+        income = float(input("Enter income for month {}:".format(month)))
         incomes.append(income)
-    print_report(number_of_months, incomes)
+    print_report(incomes)
 
 
-def print_report(number_of_months, incomes):
+def print_report(incomes):
+    """Display income report for incomes over a given number of months."""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month in range(1, len(incomes) + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
