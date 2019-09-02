8e973b1e32554709d715f959c797715c15fcf8a8
Matt <matt.evans@my.jcu.edu.au>
2019-09-02 22:52:50 +1000

    Print the string count in alignment

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,10 +1,14 @@
 number_string_occurrence = {}
 text = input("Text: ")
 strings = text.split()
-for string in strings:
-    number_string_occurrence[string] = number_string_occurrence.get(string,0)+1
+max_string_length = 0
 
-sorted_strings= sorted(number_string_occurrence)
+for string in strings:
+    number_string_occurrence[string] = number_string_occurrence.get(string, 0) + 1
+    if len(string) > max_string_length:
+        max_string_length = len(string)
+
+sorted_strings = sorted(number_string_occurrence)
 
 for string in sorted_strings:
-    print('{} : {}'.format(string,number_string_occurrence[string]))
+    print('{:{}} : {}'.format(string, max_string_length, number_string_occurrence[string]))
