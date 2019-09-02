2f7ebe0dc39c27611a6567d3858854045dab863f
Matt <matt.evans@my.jcu.edu.au>
2019-09-02 22:42:55 +1000

    Change print string count to sort alphabetic order

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,7 +4,7 @@ strings = text.split()
 for string in strings:
     number_string_occurrence[string] = number_string_occurrence.get(string,0)+1
 
+sorted_strings= sorted(number_string_occurrence)
 
-
-for string_name, string_count in number_string_occurrence.items():
-    print('{} : {}'.format(string_name,string_count))
+for string in sorted_strings:
+    print('{} : {}'.format(string,number_string_occurrence[string]))
