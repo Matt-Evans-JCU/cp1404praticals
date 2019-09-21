794f8dae567c02db38dda7cddd87fbbfdd97e280
Matt <matt.evans@my.jcu.edu.au>
2019-09-21 21:00:54 +1000

    Reformat code to meet stage 2 requirements

--- prac_07/miles_to_km.py
+++ prac_07/miles_to_km.py
@@ -4,6 +4,7 @@ from kivy.core.window import Window
 from kivy.app import StringProperty
 
 __author__ = 'Matt Evans'
+KM_CONVERSION = 1.60934
 
 
 class ConvertNumberApp(App):
@@ -17,11 +18,22 @@ class ConvertNumberApp(App):
         self.root = Builder.load_file('miles_to_km.kv')
         return self.root
 
-    def handle_increment(self,increment):
-        self.root.ids.input_number.text = str(int(self.root.ids.input_number.text) + increment)
+    def handle_increment(self, increment):
+        value = self.get_valid_miles() + increment
+        self.root.ids.input_number.text = str(value)
+        self.handle_convert()
 
     def handle_convert(self):
-        self.message = str(int(self.root.ids.input_number.text) * 1.60934)
+        value = self.get_valid_miles()
+        result = value * KM_CONVERSION
+        self.root.ids.output_number.text = str(result)
+
+    def get_valid_miles(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
 
 
 ConvertNumberApp().run()
