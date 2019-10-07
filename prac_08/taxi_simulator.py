0554c96da9e424ee044e2df9600e077443049164
Matt <matt.evans@my.jcu.edu.au>
2019-10-08 09:27:40 +1000

    Fix up mistakes

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,12 +1,13 @@
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
+MENU = """q)uit, c)hoose taxi, d)rive"""
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     bill = 0
     current_taxi = None
     print("Let's drive!")
-    menu = """q)uit, c)hoose taxi, d)rive"""
+    print(MENU)
     choice = input('>>> ').upper()
     while choice != 'Q':
         if choice == 'C':
@@ -16,13 +17,14 @@ def main():
             current_taxi = int(input('Choose taxi: '))
             print('Bill to date: ${:.2f}'.format(bill))
         if choice == 'D':
-            drive_distance = int(input('Drive how far? '))
-            taxis[current_taxi].drive(drive_distance)  # This updates self.current_fare_distance for driven distance
-            cost_of_trip = taxis[current_taxi].get_fare()
-            print('Your {} trip cost you ${:.2f}'.format(taxis[current_taxi].name,cost_of_trip))
-            bill += cost_of_trip
-            print('Bill to date: ${:.2f}'.format(bill))
-        menu = """q)uit, c)hoose taxi, d)rive"""
+            if current_taxi != None:
+                drive_distance = int(input('Drive how far? '))
+                taxis[current_taxi].drive(drive_distance)  # This updates self.current_fare_distance for driven distance
+                cost_of_trip = taxis[current_taxi].get_fare()
+                print('Your {} trip cost you ${:.2f}'.format(taxis[current_taxi].name,cost_of_trip))
+                bill += cost_of_trip
+                print('Bill to date: ${:.2f}'.format(bill))
+        print(MENU)
         choice = input('>>> ').upper()
     print('Total trip cost: ${:.2f}'.format(bill))
     print('Taxis are now:')
--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -5,15 +5,16 @@ Car class
 from prac_08.car import Car
 import random
 
+
 class UnreliableCar(Car):
 
-    def __init__(self,name,fuel,reliability):
-        super().__init__(name,fuel)
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
         """Drive like parent Car but calculate fare distance as well."""
-        random_number = random.randint(0,101)
+        random_number = random.uniform(0, 100)
         if random_number >= self.reliability:
             distance = 0
         distance_driven = super().drive(distance)
