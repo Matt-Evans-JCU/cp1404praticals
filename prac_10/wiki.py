a2f619aedbda346233c177f92d036cdd5181ba21
Matt <matt.evans@my.jcu.edu.au>
2019-10-28 14:27:07 +1000

    Finished

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,5 +2,13 @@ import wikipedia
 
 choice = input('Pick a Search Phrase or Page Title: ')
 while choice != '':
-    print(wikipedia.summary(choice))
-    choice = input('Pick a Search Phrase or Page Title: ')
+    try:
+        wiki_page = wikipedia.page(choice)
+        print(wiki_page.title)
+        print(wiki_page.summary)
+        print(wiki_page.url)
+        choice = input('Pick a Search Phrase or Page Title: ')
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("These are your options")
+        print(e.options)
+        choice = input('Pick a Search Phrase or Page Title: ')
