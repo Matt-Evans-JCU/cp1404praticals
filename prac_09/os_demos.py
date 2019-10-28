d383c7412d3ca70099ab1e13b22d3dd786e5e445
Matt <matt.evans@my.jcu.edu.au>
2019-10-28 12:40:44 +1000

    Prac 09

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-
+import shutil
 import os
 
 
@@ -17,6 +17,8 @@ def main():
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
     # Make a new directory
+    # The next time you run this, it will crash if the directory exists
+    # TODO: Use exception handling to avoid the crash (just pass)
     try:
         os.mkdir('temp')
     except FileExistsError:
@@ -31,6 +33,19 @@ def main():
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
 
+        # TODO: Try these options one at a time
+        # Option 1: rename file to new name - in place
+        # os.rename(filename, new_name)
+
+        # Option 2: move file to new place, with new name
+        # shutil.move(filename, 'temp/' + new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
 
 def demo_walk():
     """Process all subdirectories using os.walk()."""
@@ -40,34 +55,13 @@ def demo_walk():
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
+
+        # TODO: add a loop to rename the files
         for filename in filenames:
             full_name = os.path.join(directory_name, filename)
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
             os.rename(full_name, new_name)
-    # filename = 'ItIsWell (oh my soul).txt'
-    # get_fixed_filename(filename)
 
 
-def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
-
-    new_filename = ''
-    number_of_splits = 0
-    for i, element in enumerate(filename):
-        if i == (len(filename) - 1-number_of_splits):
-            pass
-        elif element == ' ' and filename[i+1+number_of_splits].islower():
-            filename = filename[:i+1+number_of_splits] + filename[i+1+number_of_splits].upper() + filename[i+2+number_of_splits:]
-        elif element == "(" and filename[i+1+number_of_splits].islower():
-            filename = filename[:i+1+number_of_splits] + filename[i+1+number_of_splits].upper() + filename[i+2+number_of_splits:]
-        elif element.islower() and filename[i + 1+number_of_splits].isupper():
-            filename = filename[:i + 1+number_of_splits] + ' ' + filename[i+1+number_of_splits:]
-            number_of_splits +=1
-
-    if new_filename == '':
-        new_filename = filename
-    new_name = new_filename.replace(" ", "_").replace(".TXT", ".txt")
-    print(new_name)
-
-
+#main()
 demo_walk()
